dd1028309d1b45a1bf851d6e50bde54de66d6168
Zach Doty <zach.doty@gmail.com>
2012-12-15 12:22:00 -0800

    salt & seed for cake

--- app/Config/core.php
+++ app/Config/core.php
@@ -185,6 +185,7 @@
  * A random string used in security hashing methods.
  */
 	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
