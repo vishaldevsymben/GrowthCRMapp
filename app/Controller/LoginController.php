1beddde5f541b5f1aa8b64dc02b653b0d96e867a
Zach Doty <zach.doty@gmail.com>
2012-12-15 22:35:11 -0800

    Testing

---
+++ app/Controller/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+class LoginController extends AppController {
+
+	public $helpers = array('Form', 'Html', 'Js', 'Time');
+
+	public function index() {
+
+	}
+	
+    public function view($id) {
+        //action logic goes here..
+    }
+
+    public function share($customerId, $recipeId) {
+        //action logic goes here..
+    }
+
+    public function search($query) {
+        //action logic goes here..
+    }
+}
+
+?>
--- app/View/Login/index.ctp
+++ app/View/Login/index.ctp
@@ -11,3 +11,4 @@ Forgot username or password.<br>
 
 
 Bottom of page: Don't have a GrowthCRM account? Sign up today. (link)
+
